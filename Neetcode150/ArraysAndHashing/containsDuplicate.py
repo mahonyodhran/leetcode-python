ce441ee5ca0d2f5a84b8f355fb8f6c34146ba756
Odhran <mahonyodhran@gmail.com>
2022-10-03 19:32:57 +0100

    added question

--- Neetcode150/ArraysAndHashing/containsDuplicate.py
+++ Neetcode150/ArraysAndHashing/containsDuplicate.py
@@ -1,3 +1,12 @@
+'''Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+Input: nums = [1,2,3,1]
+Output: true
+
+Input: nums = [1,2,3,4]
+Output: false
+'''
+
 class Solution(object):
     def containsDuplicate(self, nums):
         """
