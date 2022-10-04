f4b357d8682457544685e1dad947f50e64ce8401
Odhran <mahonyodhran@gmail.com>
2022-10-04 18:35:49 +0100

    added Neetcode hashmap solution

--- ArraysAndHashing/validAnagram.py
+++ ArraysAndHashing/validAnagram.py
@@ -20,14 +20,32 @@ class Solution:
         
         return listS == listT
 
+    def isAnagramHashmap(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        
+        countS, countT = {}, {}
+        
+        for i in range(len(s)):
+            countS[s[i]] = 1 + countS.get(s[i], 0)
+            countT[t[i]] = 1 + countT.get(t[i], 0)
+        
+        for c in countS:
+            if countS[c] != countT.get(c, 0):
+                return False
+        
+        return True
+
 sol = Solution()
 
 s = "anagram"
 t = "nagaram"
 
 print(sol.isAnagram(s, t))
+print(sol.isAnagramHashmap(s, t))
 
 s = "rat"
 t = "car"
 
 print(sol.isAnagram(s, t))
+print(sol.isAnagramHashmap(s, t))
