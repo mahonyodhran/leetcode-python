5d0932a55b3cfa96ff6b6fbd3ae94c0c0a8b3583
Odhran <mahonyodhran@gmail.com>
2022-10-04 18:18:39 +0100

    added answer - done by converting to list, sorting, and checking if they are equal

--- ArraysAndHashing/validAnagram.py
+++ ArraysAndHashing/validAnagram.py
@@ -13,11 +13,21 @@ Output: false
 
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        pass
-    
+        listS = list(s)
+        listS.sort()
+        listT = list(t)
+        listT.sort()
+        
+        return listS == listT
+
 sol = Solution()
 
 s = "anagram"
 t = "nagaram"
+
+print(sol.isAnagram(s, t))
+
+s = "rat"
+t = "car"
 
 print(sol.isAnagram(s, t))
