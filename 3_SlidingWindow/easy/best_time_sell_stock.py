5ef8bfa0c1ba949bd36cb0ae2bc96d0292780009
Odhran <mahonyodhran@gmail.com>
2022-10-13 21:22:47 +0100

    added boilerplate stuff

--- 3_SlidingWindow/easy/best_time_sell_stock.py
+++ 3_SlidingWindow/easy/best_time_sell_stock.py
@@ -13,8 +13,8 @@ Input: prices = [7,1,5,3,6,4]
 Output: 5
 Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
 Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
-Example 2:
 
+Example 2:
 Input: prices = [7,6,4,3,1]
 Output: 0
 Explanation: In this case, no transactions are done and the max profit = 0.
@@ -22,4 +22,9 @@ Explanation: In this case, no transactions are done and the max profit = 0.
 
 class Solution:
     def maxProfit(self, prices: list[int]) -> int:
-        pass
+        return 0
+    
+sol = Solution()
+
+print(sol.maxProfit([7,1,5,3,6,4])) #5
+print(sol.maxProfit([7,6,4,3,1])) #0
